da13a9ac26b9f944125be3e362ca5fff89998b56
Isaac Yaw Tawiah <isaacyawtawiah@gmail.com>
2023-03-14 13:57:37 +0000

    My first react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <h1 style={{backgroudColor:'voilet',color:'red'}}>Codetrain is awesome, react works!!!</h1>
+    </div>
+  )
+}
+
+export default App
